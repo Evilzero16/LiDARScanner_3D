e7c3e65a09cd9f179cfafaf5a59c1d446b8deb8f
Evilzero16 <hrishitkotadia240@gmail.com>
2025-03-12 13:46:55 -0400

    Initial Commit

---
+++ LiDARScanner_3D/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiDARScanner_3D
+//
+//  Created by Hrishit Kotadia on 3/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LiDARScanner_3DTests/LiDARScanner_3DTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiDARScanner_3DTests.swift
+//  LiDARScanner_3DTests
+//
+//  Created by Hrishit Kotadia on 3/12/25.
+//
+
+import Testing
+@testable import LiDARScanner_3D
+
+struct LiDARScanner_3DTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
